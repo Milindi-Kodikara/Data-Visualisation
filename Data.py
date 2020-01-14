9de6532f30e3a38dfdc8134b6ce23547a6ed9552
Milindi-Kodikara <s3667779@student.rmit.edu.au>
2020-01-14 16:59:06 +1100

    Write to csv

--- Data.py
+++ Data.py
@@ -1,16 +1,24 @@
 import pandas as pd
+
 print("Keywords of Fun Palace")
 #Dictionary of Lists
-#data = {'Name': ['Tom', 'Mili', 'WomBOI', '\'Burt!', 'Squank'],
-# 'Type': ['Peasant', 'Pug', 'Wombat', 'Machine Learning Bot', 'Chat Bot']}
+dataInitial = pd.DataFrame({'Id': [1, 2, 3, 4, 5], 'Name': ['Tom', 'Mili', 'WomBOI', '\'Burt!', 'Squank'],
+ 'Type': ['Peasant', 'Pug', 'Wombat', 'Machine Learning Bot', 'Chat Bot']})
+
+dataTwo = pd.DataFrame({'Id': [1, 2, 3, 4, 5],
+           'Production Value': [0.987857, 0.9999999, 0.87566, 0.98643, 0.766578]})
 
 #List of dictionaries
-data = [{'Name': 'Tom', 'Type': 'Peasant', 'But really': 'Human', 'Rating in WomhB': 5},
-        {'Name': 'Mili', 'Type': 'Pug', 'But really': 'Human', 'Rating in WomhB': 5},
-        {'Name': 'WomBOI', 'Type': 'Wombat', 'Rating in WomhB': 4},
-        {'Name': '\'Burt!', 'Type': 'Machine Learning Bot', 'Rating in WomhB': 3},
-        {'Name': 'Squank', 'Type': 'Chat Bot', 'Rating in WomhB': 2.5}]
+data = [{'id': 1, 'Name': 'Tom', 'Type': 'Peasant', 'But really': 'Human', 'Rating in WomhB': 5},
+        {'id': 2, 'Name': 'Mili', 'Type': 'Pug', 'But really': 'Human', 'Rating in WomhB': 5},
+        {'id': 3, 'Name': 'WomBOI', 'Type': 'Wombat', 'Rating in WomhB': 4},
+        {'id': 4, 'Name': '\'Burt!', 'Type': 'Machine Learning Bot', 'Rating in WomhB': 3},
+        {'id': 5, 'Name': 'Squank', 'Type': 'Chat Bot', 'Rating in WomhB': 2.5}]
+
 df = pd.DataFrame(data, index=['1', '2', '3', '4', '5'])
+#dfIntial = pd.DataFrame(dataInitial, index=['1', '2', '3', '4', '5'])
+#dfTwo = pd.DataFrame(data, index=['1', '2', '3', '4', '5'])
+
 print(df)
 print()
 print("A glimpse to all our characters: ")
@@ -54,3 +62,13 @@ print()
 #Group data by ranking
 print("Ranks!")
 print(df.groupby('Rating in WomhB').groups)
+print()
+
+print("Here we look at Production Value:")
+print(pd.merge(dataInitial, dataTwo, on='Id'))
+print()
+
+df.to_csv('fun_palace.csv', sep=',', encoding='utf-8')
+
+print()
+print(pd.datetime.now())
