767a2d1cb11348d44b64c852f0117b4c5ea5ecd4
Milindi-Kodikara <s3667779@student.rmit.edu.au>
2020-01-14 16:17:32 +1100

    Added stats

--- Data.py
+++ Data.py
@@ -5,11 +5,11 @@ print("Keywords of Fun Palace")
 # 'Type': ['Peasant', 'Pug', 'Wombat', 'Machine Learning Bot', 'Chat Bot']}
 
 #List of dictionaries
-data = [{'Name': 'Tom', 'Type': 'Peasant', 'But really': 'Human'},
-        {'Name': 'Mili', 'Type': 'Pug', 'But really': 'Human'},
-        {'Name': 'WomBOI', 'Type': 'Wombat'},
-        {'Name': '\'Burt!', 'Type': 'Machine Learning Bot'},
-        {'Name': 'Squank', 'Type': 'Chat Bot'}]
+data = [{'Name': 'Tom', 'Type': 'Peasant', 'But really': 'Human', 'Rating in WomhB': 5},
+        {'Name': 'Mili', 'Type': 'Pug', 'But really': 'Human', 'Rating in WomhB': 5},
+        {'Name': 'WomBOI', 'Type': 'Wombat', 'Rating in WomhB': 4},
+        {'Name': '\'Burt!', 'Type': 'Machine Learning Bot', 'Rating in WomhB': 3},
+        {'Name': 'Squank', 'Type': 'Chat Bot', 'Rating in WomhB': 2.5}]
 df = pd.DataFrame(data, index=['1', '2', '3', '4', '5'])
 print(df)
 print()
@@ -22,8 +22,35 @@ df['Feature'] = ['General', 'General', 'General', 'Fud', 'SquankApp']
 print(df)
 print()
 #Selecting a row of data
-print("Guess who uses the measuring unit WohmB everyday!")
+print("Guess who uses the measuring unit WohmB everyday! This guuuyyy:")
 print(df[2:3])
 print()
 print("If you want their full details in another way:")
 print(df.iloc[2])
+print()
+
+print("The actual data in our data frame is:")
+print(df.values)
+print()
+
+print("The mean value in WomhBs")
+print(df.mean())
+print()
+
+print("Summary of statistical data:")
+print(df.describe())
+print()
+#Iterating the dataframe coz why not
+print("Details in a concise way:")
+for row_index, row in df.iterrows():
+        print(row)
+        print()
+
+#Check for any missed data
+print("Missing data?!")
+print(df['But really'].isnull())
+print()
+
+#Group data by ranking
+print("Ranks!")
+print(df.groupby('Rating in WomhB').groups)
